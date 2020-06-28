88b06c2a5d883f5bc451127993ace11838b70252
haysch <36488481+haysch@users.noreply.github.com>
2020-06-28 21:50:52 +0200

    Remove unneeded shuf and port exposing

--- tests/run-client-container.sh
+++ tests/run-client-container.sh
@@ -5,8 +5,7 @@ set -e
 RUN_OPTS="-d --cap-add=NET_ADMIN --device=/dev/net/tun --tmpfs /run --tmpfs /run/lock -v /sys/fs/cgroup:/sys/fs/cgroup:ro"
 
 setup_container() {
-  ssh_port=$(shuf -i 22222-44444 -n 1)
-  container_id=$(docker run ${RUN_OPTS} -p $ssh_port:22 --name $3 ansible-ovpn-infra/$1:$2)
+  container_id=$(docker run ${RUN_OPTS} --name $3 ansible-ovpn-infra/$1:$2)
   container_ip=$(docker inspect -f '{{ .NetworkSettings.IPAddress }}' ${container_id})
 
   echo "$3 ansible_host=$container_ip ansible_user=docker"
